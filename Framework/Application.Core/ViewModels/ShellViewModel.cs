7518880f8df01e2347b029265ddab266c5728dfa
peter-durrant <peter-durrant@users.noreply.github.com>
2020-01-25 10:39:40 +0000

    Move view registration to shell ctor

--- Framework/Application.Core/ViewModels/ShellViewModel.cs
+++ Framework/Application.Core/ViewModels/ShellViewModel.cs
@@ -1,21 +1,8 @@
-﻿using System;
-using Hdd.Application.Core.Regions;
-using Hdd.Application.Core.Views;
-using Prism.Mvvm;
-using Prism.Regions;
+﻿using Prism.Mvvm;
 
 namespace Hdd.Application.Core.ViewModels
 {
     public class ShellViewModel : BindableBase
     {
-        public ShellViewModel(IRegionManager regionManager)
-        {
-            _ = regionManager ?? throw new ArgumentNullException(nameof(regionManager));
-
-            regionManager.RegisterViewWithRegion(RegionNames.TabRegion, typeof(DefaultView));
-            regionManager.RegisterViewWithRegion(RegionNames.ContentRegion, typeof(DefaultView));
-            regionManager.RegisterViewWithRegion(RegionNames.NavigationRegion, typeof(DefaultView));
-            regionManager.RegisterViewWithRegion(RegionNames.NotificationRegion, typeof(DefaultView));
-        }
     }
 }
--- Framework/Application.Core/Views/Shell.xaml.cs
+++ Framework/Application.Core/Views/Shell.xaml.cs
@@ -1,12 +1,22 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
+using Hdd.Application.Core.Regions;
+using Prism.Regions;
 
 namespace Hdd.Application.Core.Views
 {
     public partial class Shell : Window
     {
-        public Shell()
+        public Shell(IRegionManager regionManager)
         {
             InitializeComponent();
+
+            _ = regionManager ?? throw new ArgumentNullException(nameof(regionManager));
+
+            regionManager.RegisterViewWithRegion(RegionNames.TabRegion, typeof(DefaultView));
+            regionManager.RegisterViewWithRegion(RegionNames.ContentRegion, typeof(DefaultView));
+            regionManager.RegisterViewWithRegion(RegionNames.NavigationRegion, typeof(DefaultView));
+            regionManager.RegisterViewWithRegion(RegionNames.NotificationRegion, typeof(DefaultView));
         }
     }
 }
